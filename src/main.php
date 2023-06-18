4fe46e7e7c9e47d4b619a6f5f89b5a656706e746
emmahy-park <emma95@student.ubc.ca>
2023-06-17 22:58:06 -0700

    created src folder

---
+++ src/main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="main.css">
+        <title>Lab Inventory Management System</title>
+    </head>
+
+    <body>
+        <nav>
+            <label class="logo">Lab Inventory Management System</label>
+            <ul>
+                <li><a href="#">LAB</a></li>
+                <li><a href="#">CURRENT STOCK</a></li>
+                <li><a href="#">PURCHASE</a></li>
+                <li><a href="#">VENDOR</a></li>
+                <li><a href="#">WASTE</a></li>
+                <li><a href="#">MEMBERS</a></li>
+            </ul>
+        </nav>
+    </body>
+
+
+</html>
