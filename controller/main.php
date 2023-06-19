2bf20ec1afd7f760836226a1fff398d57af38a14
shumin11 <wsmin123456@hotmail.com>
2023-06-18 18:37:16 -0700

    add new files

--- controller/main.php
+++ controller/main.php
@@ -20,27 +20,57 @@
         <div class="d-flex">
             <div class="leftpane">
                 <div class = "sidebar">
-                    <ul>
-                        <li><a href="Lab.php">LAB</a></li>
+                    
+
+                    <form method="POST" action="main.php">
+                        <li>
+                            <input type="submit" name="teamTag" value="Lab" />
+                        </li>
+
+                        <li>
+                        <input type="submit" name="currentStockTag" value="CURRENT STOCK" />
+                        </li>
+
+                        <li>
+                        <input type="submit" name="purchaseTag" value="PURCHASE" />
+                        </li>
+
+                        <li>
+                        <input type="submit" name="vendorTag" value="VENDOR" />
+                        </li>
+
+                        <li>
+                        <input type="submit" name="wasteTag" value="WASTE" />
+                        </li>
+
+                        <li>
+                        <input type="submit" name="memberTag" value="MEMBERS" />
+                        </li>           
+                     </form>
+
+                        <!-- <li>
+                            <a href="Lab.php">LAB</a>
+                        </li>
                         <li><a href="CurrentStock.php">CURRENT STOCK</a></li>
                         <li><a href="Purchase.php">PURCHASE</a></li>
                         <li><a href="Vendor.php">VENDOR</a></li>
                         <li><a href="Waste.php">WASTE</a></li>
-                        <li><a href="Members.php">MEMBERS</a></li>
-                    </ul>
+                        <li><a href="Members.php">MEMBERS</a></li> -->
+                    
                 </div>
                  
-
-
-
 
              </div>
 
             <div class="middlepane">
                 Test Page
+
+
+
             </div>
 
             <div class="rightpane">
+                
             <h2>Insert </h2>
                 <form method="POST" action="oracle-test.php"> <!--refresh page when submitted-->
                     <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
